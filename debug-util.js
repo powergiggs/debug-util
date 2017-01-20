1dbaa1efe483728eb581baed8f824f3ee0c9beb2
ecorreia design <el4591@hotmail.com>
2017-01-19 22:20:52 -0500

    utility tool pass unit test 100%

--- debug-util.js
+++ debug-util.js
@@ -4,29 +4,38 @@ const callerId = require('caller-id');
 
 const date = new Date();
 
+const proc = process.argv;
+
 function grab(param) {
-  const i = process.argv.indexOf(param);
+  const i = proc.indexOf(param);
   return (i === -1 ? null : process.argv[i + 1]);
 }
 let DEBUG = grab('--debug') || grab('-d');
 if (DEBUG === 'true'){
   DEBUG = true;
+  process.env.DEBUG = 'true'
 }
 
-function debug(msg, type, data) {
+function debug(msg, type, data, callback) {
   const who = callerId.getData();
   fs.access('./logs', (err) => {
     if(err){
-      console.log(err);
+      fs.mkdirSync('./logs');
     }else{
-      if(DEBUG === true){
 
-        if(type > 2 || typeof type == "string") {
-          console.log(`type of ${type} is invalid. DEBUG module crashed! valid: int[0,1,2]`);
-          return;
-        }
+      if(!msg){
+        console.log('You must specify a message THE LEAST. DEBUG module crashed!');
+        if(callback) return callback(0);
+      }
 
-        if(typeof data === 'object'){
+      if(type > 2 || typeof type == "string") {
+        console.log(`type of ${type} is invalid. DEBUG module crashed! valid: int[0,1,2]`);
+        if(callback) return callback(0);
+      }
+
+      if(process.env.DEBUG === 'true') {
+
+        if (typeof data === 'object') {
           data = JSON.stringify(data);
         }
 
@@ -52,14 +61,16 @@ function debug(msg, type, data) {
 
         const time = `Time: ${date.getHours()}:${date.getMinutes()} ${date.getSeconds()}`;
 
-        colorAndConsole(logMsg, time);
-        logTofile(logMsg, time);
+        colorAndConsole(logMsg, time, callback);
+      }else{
+        console.log(`You did not turn the DEBUG mode ON`);
+        if(callback) return callback(0);
       }
     }
-  });
+    });
 }
 
-function colorAndConsole(msg, time) {
+function colorAndConsole(msg, time, callback) {
 
   const mode = '\n-- Debugging -----------------';
   const blueb = chalk.bold.blue;
@@ -81,9 +92,13 @@ function colorAndConsole(msg, time) {
   ${yellowb('File')}: ${yellow(msg.path)}
   Func: ${magenta(msg.func)} | type: ${magenta(msg.ftyp)} | method: ${magenta(msg.meth)}
   ${blueb('Data')}: ${typeof msg.data} ${blue(msg.data)}\n`);
+
+  logTofile(msg, time, callback);
+
+  // if(callback) return callback(1);
 }
 
-function logTofile(msg, time) {
+function logTofile(msg, time, callback) {
   const logMsg =
 `${time}
 ${msg.type}: ${msg.mesg}
@@ -94,11 +109,13 @@ Func: ${msg.func} | type: ${msg.ftyp} | method: ${msg.meth}
   const file = `log_${date.getMonth() + 1}_${date.getDate()}_${date.getFullYear()}_${date.getHours()}.log`;
   const title = `================ Log of ${date.getMonth() + 1} ${date.getDate()} ${date.getFullYear()} Hour: ${date.getHours()} =====`;
 
-  if (!fs.existsSync(`./logs/${file}`)) {
-    fs.writeFileSync(`logs/${file}`, title);
-  }
-
   fs.appendFileSync(`./logs/${file}`, `\n\n${logMsg.trim()}`);
+
+  if(callback) return callback(1);
 }
 
-module.exports = debug;
+module.exports.debug = debug;
+module.exports.logTofile = logTofile;
+module.exports.colorAndConsole = colorAndConsole;
+module.exports.grab = grab;
+module.exports.process = proc;
---
+++ test/debugTest.js
@@ -0,0 +1,109 @@
+const chai = require('chai')
+  , expect = chai.expect
+  , should = chai.should();
+const rewire = require('rewire');
+const sinon = require('sinon');
+const util = rewire('../debug-util');
+
+const date = new Date();
+
+describe('Debug Utility', () => {
+
+  let msg, time, type, data, ret;
+
+  before((done)=>{
+
+    this.console = {
+      log: sinon.spy()
+    };
+
+    util.__set__('console', this.console);
+
+    msg = {
+      type: 'Warn',
+      mesg: 'utility tool',
+      path: '/Users/usrname/www/_tests/debugTool/test.js',
+      line: 4,
+      func: null,
+      ftyp: 'Object',
+      meth: null,
+      data: 'null' };
+
+    time = `Time: ${date.getHours()}:${date.getMinutes()} i${date.getSeconds()}`;
+
+    type = 1;
+
+    data = ['unit','testing'];
+
+    ret = 'temp';
+
+    done()
+
+  });
+
+  it('Grab function should return null', function () {
+    const debug = util.grab('--debug');
+    expect(debug).to.equal(null)
+  });
+
+  it('logTofile function should return 1', function (done) {
+
+    const debug = util.logTofile(msg, time, (res) => {
+      return res
+    } );
+
+    expect(debug).to.equal(1);
+
+    done()
+
+  });
+
+  it('colorAndConsole function should return 1 if debug mode is on', function (done) {
+
+    const debug = util.colorAndConsole(msg, time, (res) => {
+      return res
+    } );
+
+    expect(debug).to.equal(undefined);
+
+    done()
+
+  });
+
+  it('debug function should return 0 if debug mode is off', function (done) {
+    process.env.DEBUG = 'false';
+    util.process = ['--debug','false'];
+    util.debug('testing', type, data, (res) => {
+      res.should.equal(0);
+      done();
+    } );
+  });
+
+  it('debug function should return 1', function (done) {
+    process.env.DEBUG = 'true';
+    util.process = ['--debug','true'];
+    util.debug('testing', type, data, (res) => {
+      res.should.equal(1);
+      done();
+    } );
+  });
+
+  it('debug function should return 0 if message is not defined', function (done) {
+    process.env.DEBUG = 'true';
+    util.process = ['--debug','true'];
+    util.debug('', type, data, (res) => {
+      res.should.equal(0);
+      done();
+    } );
+  });
+
+  it('debug function should return 0 if type is over 2', function (done) {
+    process.env.DEBUG = 'true';
+    util.process = ['--debug','true'];
+    util.debug('', 1 , data, (res) => {
+      res.should.equal(0);
+      done();
+    } );
+  });
+});
+
